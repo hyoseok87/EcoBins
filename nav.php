b65398a6ef016c574918ce7785c304658e825c10
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-06-21 10:07:50 +0200

    Mobile Version erstellt

--- nav.php
+++ nav.php
@@ -4,15 +4,11 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 ?>
 <nav>
-    <input id="menuToggle" type="checkbox" style="display:none;">
-    <label id="menuButton" for="menuToggle" style="display:none; flex;">
-        <div id="menuButtonIcon" style="width:24px;padding:8px;">
-            <svg viewBox="0 0 24.0 24.0" preserveAspectRatio="none" style="background:url('data:image/png;base64,');">
-                <path d="M5.75 5.25h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 0 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5z"></path>
-            </svg>
-        </div>
+    <input type="checkbox" id="menuToggle" style="display:none;">
+    <label for="menuToggle" id="menuButton">
+        <div id="menuButtonIcon">&#9776;</div>
     </label>
-    <ul id="verticalMenu" style="display: flex; list-style: none; margin: 0; padding: 0;">
+    <ul id="verticalMenu">
         <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" href="index.php" title="Home">Home</a></li>
         <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'service.php' ? 'active' : ''; ?>" href="service.php" title="Service">Service</a></li>
         <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'merkmale.php' ? 'active' : ''; ?>" href="merkmale.php" title="Merkmale">Merkmale</a></li>
@@ -27,8 +23,8 @@ if (session_status() == PHP_SESSION_NONE) {
             <li class="navMenuLink"><a class="navMenuLinkContent" href="logout.php" title="Abmelden" onclick="return confirmLogout();">Abmelden</a></li>
         <?php else: ?>
             <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'login.php' ? 'active' : ''; ?>" href="login.php" title="Anmelden">Anmelden</a></li>
+            <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'register.php' ? 'active' : ''; ?>" href="register.php" title="Registrieren">Registrieren</a></li>
         <?php endif; ?>
-        <li class="navMenuLink"><a class="navMenuLinkContent <?php echo basename($_SERVER['PHP_SELF']) == 'register.php' ? 'active' : ''; ?>" href="register.php" title="Register">Registrieren</a></li>
     </ul>
 </nav>
 
